03a15e9b19ce3a4acd0a851aaae0b04359abd799
Hyeonchul <wkdgus1136@naver.com>
2023-08-14 14:47:23 +0900

    feature: 비즈니스 로직 및 메인서버와 컴파일서버 분리

--- src/main/java/com/algorithm/algoprojectserver/compiler/JavaCompile.java
+++ src/main/java/com/algorithm/algoprojectserver/compiler/JavaCompile.java
@@ -8,12 +8,17 @@ import com.algorithm.algoprojectserver.service.ProblemService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import java.io.*;
 import java.lang.reflect.Method;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
@@ -22,116 +27,82 @@ import java.util.List;
 /**
  * File Name : JavaCompile
  * Description : 클라이언트에서 받은 JAVA언어의 알고리즘을 컴파일하고 검증
- * Update : 2023-08-11
+ * Update : 2023-08-14
  */
+
+/*
+* 클라이언트에서 받은 사용자의 코드를 동적 컴파일 하기위해 따로 마련된 Java언어 컴파일 서버를 시켜야함
+* http://localhost:8081
+* */
 
 
 @Component
 @Slf4j
 public class JavaCompile {
 
+    private final String COMPILE_SERVER_URL = "http://localhost:8081";
+
     @Autowired
     ProblemService problemService;
 
 
-    public List<String> compileJavaCode(String code, Integer pageNum, HttpServletRequest request) {
-        int answerCount = 0;
+    public List<String> compileJavaCode(String code, Integer pageNum, HttpServletRequest servletRequest) {
+
+        /*
+        * 해당 페이지의 정답값을 불러온다음 AnswerDTO에 담는다
+        * */
+
+
         List<AnswerDTO> problemAnswers = problemService.getProblemAnswers(pageNum);
         List<String> compileResultList = new ArrayList<>();
 
+        int answerCount = 0;
+
         for (AnswerDTO problemAnswer : problemAnswers) {
             answerCount++;
+            // 컴파일 서버와 통신하기 위한 RestTemplate 객체 생성
 
-            log.info("[요청 IP : {}] JAVA언어 컴파일 요청 페이지 정보 {} 번", request.getRemoteAddr(), pageNum);
-
-            log.info("[요청 IP : {}] {}번째 컴파일 요청 수행 ", request.getRemoteAddr(), answerCount);
+            RestTemplate restTemplate = new RestTemplate();
 
             /*
-             * 해결하고자하는 문제의 정답을 DB에서 가져와 problemAnswer에 저장한뒤 반복문으로 정답수만큼 정답체크
-             * */
+            * 각각 input에는 알고리즘 검증을 위한 정답값, output에는 결과값이 들어가 시도횟수만큼 테스트를 돌린다
+            * */
+
+            URI uri = UriComponentsBuilder
+                    .fromUriString(COMPILE_SERVER_URL)
+                    .path("/")
+                    .queryParam("input", problemAnswer.getAnswer_input())
+                    .queryParam("output", problemAnswer.getAnswer_output())
+                    .encode().build().toUri();
+
+            RequestEntity<String> request = RequestEntity
+                    .post(uri)
+                    .body(code);
+
+            ResponseEntity<String> responseEntity = restTemplate.exchange(request, String.class);
+            String body = responseEntity.getBody();
+
+            log.debug("[요청 IP : {}] {}번째 컴파일 요청 수행 : 입력 코드 : {}", servletRequest.getRemoteAddr(), answerCount, code);
 
 
-            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-
-
-            /* 1. JAVA언어(.java)로 되있는 파일을 만든다 */
-
-            log.info("[요청 IP : {}] {}번째 컴파일 요청 수행 : .java언어 파일 생성", request.getRemoteAddr(), pageNum);
-
-            File sourceFile = new File("Main.java");
-            PrintStream save = System.out;
-
-            try (FileWriter writer = new FileWriter(sourceFile)) {
-                writer.write(code);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-            log.info("[요청 IP : {}] {}번째 컴파일 요청 수행 : 요청한 JAVA언어 파일 컴파일 수행", request.getRemoteAddr(), pageNum);
-
-            /* 2. 로컬 (배포) 환경에 설치되있는 컴파일러의 기능을 통해 해당 파일 컴파일 수행 */
-
-            int run = compiler.run(null, null, null, sourceFile.getPath());
-
-            System.setIn(new ByteArrayInputStream(problemAnswer.getAnswer_input().getBytes()));
-
-
-            try (URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{new File("").toURI().toURL()})) {
-                if (run != 0) {
-                    throw new Exception("Compile Error!");
-                }
-
-                Class<?> userClass = classLoader.loadClass("Main");
-
-
-                Object instance = userClass.getDeclaredConstructor().newInstance();
-
-
-                Method runMethod = userClass.getMethod("main");
-
-
-                ByteArrayOutputStream out = new ByteArrayOutputStream();
-                PrintStream printStream = new PrintStream(out);
-                System.setOut(printStream);
-
-
-                runMethod.invoke(instance);
-
-
-                System.setOut(save);
-                printStream.close();
-
-
-                String result = out.toString().trim();
-
-
-                /* 3. 컴파일한 결과값을 대조하고 결과값을 compileResultList에 담는다 */
-
-                if (result.equals(problemAnswer.getAnswer_output())) {
+                if (body.equals("Success")) {
                     compileResultList.add(CompileConstains.COMPILE_SUCCESS);
 
-                    log.info("[요청 IP : {}] {}번째 컴파일 요청 수행 : 컴파일 결과 : 성공", request.getRemoteAddr(), pageNum);
+                    log.info("[요청 IP : {}] {}번째 컴파일 요청 수행 : 컴파일 결과 : 성공", servletRequest.getRemoteAddr(), answerCount);
 
                 }
 
-                if (!result.equals(problemAnswer.getAnswer_output())) {
+            if (body.equals("Fail")) {
                     compileResultList.add(CompileConstains.COMPILE_FAIL);
 
-                    log.info("[요청 IP : {}] {}번째 컴파일 요청 수행 : 컴파일 결과 : 실패", request.getRemoteAddr(), pageNum);
+                    log.info("[요청 IP : {}] {}번째 컴파일 요청 수행 : 컴파일 결과 : 실패", servletRequest.getRemoteAddr(), answerCount);
 
                 }
 
-
-            } catch (Exception e) {
+            if (body.equals("Error") || body.equals("알수없는 오류")) {
                 compileResultList.add(CompileConstains.COMPILE_ERROR);
 
-                log.info("[요청 IP : {}] {}번째 컴파일 요청 수행 : 컴파일 결과 : 컴파일에러", request.getRemoteAddr(), pageNum);
-
-                break;
-            } finally {
-                sourceFile.delete();
-                new File("Main.java").delete();
+                log.info("[요청 IP : {}] {}번째 컴파일 요청 수행 : 컴파일 결과 : 컴파일 에러", servletRequest.getRemoteAddr(), answerCount);
 
             }
         }
